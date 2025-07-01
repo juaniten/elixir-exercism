e8cc456051e485b038fb5ab8ec533377b24a5804
Juan Guijarro <juanguijarroferreiro@gmail.com>
2025-07-01 14:58:43 +0200

    added lasagna solution

--- lasagna/lib/lasagna.ex
+++ lasagna/lib/lasagna.ex
@@ -1,12 +1,13 @@
 defmodule Lasagna do
-  # Please define the 'expected_minutes_in_oven/0' function
-  def expected_minutes_in_oven() do
-  
-  # Please define the 'remaining_minutes_in_oven/1' function
+  def expected_minutes_in_oven(), do: 40
 
-  # Please define the 'preparation_time_in_minutes/1' function
+  def remaining_minutes_in_oven(minutes_in_oven), do: expected_minutes_in_oven() - minutes_in_oven
 
-  # Please define the 'total_time_in_minutes/2' function
+  def preparation_time_in_minutes(added_layers), do: added_layers * 2
 
-  # Please define the 'alarm/0' function
+  def total_time_in_minutes(added_layers, minutes_in_oven) do
+    preparation_time_in_minutes(added_layers) + minutes_in_oven
+  end
+
+  def alarm(), do: "Ding!"
 end
