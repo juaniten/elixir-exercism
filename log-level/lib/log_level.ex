725c874c44456faee20bfaf83937216e1c58b918
Juan Guijarro <juanguijarroferreiro@gmail.com>
2025-07-02 09:33:43 +0200

    added log level solution

--- log-level/lib/log_level.ex
+++ log-level/lib/log_level.ex
@@ -1,9 +1,45 @@
 defmodule LogLevel do
   def to_label(level, legacy?) do
-    # Please implement the to_label/2 function
+    cond do
+      level == 0 ->
+        cond do
+          legacy? -> :unknown
+          not legacy? -> :trace
+        end
+
+      level == 1 ->
+        :debug
+
+      level == 2 ->
+        :info
+
+      level == 3 ->
+        :warning
+
+      level == 4 ->
+        :error
+
+      level == 5 ->
+        cond do
+          legacy? -> :unknown
+          not legacy? -> :fatal
+        end
+
+      true ->
+        :unknown
+    end
   end
 
   def alert_recipient(level, legacy?) do
-    # Please implement the alert_recipient/2 function
+    label = to_label(level, legacy?)
+    cond do
+      label == :error or label == :fatal -> :ops
+      label == :unknown -> cond do
+        legacy? -> :dev1
+        not legacy? -> :dev2
+      end
+      true -> :false
+    end
+
   end
 end
