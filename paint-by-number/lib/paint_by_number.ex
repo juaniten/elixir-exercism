181373ec56cca21cf4a2ccfd1de1d4aa4621583c
Juan Guijarro <juanguijarroferreiro@gmail.com>
2025-07-03 10:42:24 +0200

    added oaint by numbers solution

--- paint-by-number/lib/paint_by_number.ex
+++ paint-by-number/lib/paint_by_number.ex
@@ -1,29 +1,29 @@
 defmodule PaintByNumber do
-  def palette_bit_size(color_count) do
-    # Please implement the palette_bit_size/1 function
-  end
+  def palette_bit_size(color_count) when color_count <= 2, do: 1
+  def palette_bit_size(color_count), do: 1 + palette_bit_size(color_count / 2)
 
-  def empty_picture() do
-    # Please implement the empty_picture/0 function
-  end
+  def empty_picture(), do: <<>>
 
-  def test_picture() do
-    # Please implement the test_picture/0 function
-  end
+  def test_picture(), do: <<0b00::2, 0b01::2, 0b10::2, 0b11::2>>
 
   def prepend_pixel(picture, color_count, pixel_color_index) do
-    # Please implement the prepend_pixel/3 function
+    bit_size = palette_bit_size(color_count)
+    <<pixel_color_index::size(bit_size), picture::bitstring>>
   end
 
+  def get_first_pixel(picture, _color_count) when picture == <<>>, do: nil
   def get_first_pixel(picture, color_count) do
-    # Please implement the get_first_pixel/2 function
+    bit_size = palette_bit_size(color_count)
+    <<first::size(bit_size), _::bitstring>> = picture
+    first
   end
 
+  def drop_first_pixel(picture, _color_count) when picture == <<>>, do: <<>>
   def drop_first_pixel(picture, color_count) do
-    # Please implement the drop_first_pixel/2 function
+    bit_size = palette_bit_size(color_count)
+    <<_::size(bit_size), rest::bitstring>> = picture
+    rest
   end
 
-  def concat_pictures(picture1, picture2) do
-    # Please implement the concat_pictures/2 function
-  end
+  def concat_pictures(picture1, picture2), do: <<picture1::bitstring, picture2::bitstring>>
 end
