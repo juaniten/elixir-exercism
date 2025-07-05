5ddde3df7cfbd47e90e0174167d570ad979a4ede
Juan Guijarro <juanguijarroferreiro@gmail.com>
2025-07-05 20:39:46 +0200

    added rpg solution

--- community-garden/lib/community_garden.ex
+++ community-garden/lib/community_garden.ex
@@ -5,23 +5,36 @@ defmodule Plot do
 end
 
 defmodule CommunityGarden do
-  def start(opts) do
-    # Please implement the start/1 function
+  def start(opts \\ []) do
+    Agent.start(fn -> %{plots: [], last_id: 0} end, opts)
   end
 
   def list_registrations(pid) do
-    # Please implement the list_registrations/1 function
+    Agent.get(pid, fn state -> state.plots end)
   end
 
   def register(pid, register_to) do
-    # Please implement the register/2 function
+    Agent.get_and_update(pid, fn state ->
+      %{plots: plots, last_id: last_id} = state
+      new_id = last_id + 1
+      new_plot = %Plot{plot_id: new_id, registered_to: register_to}
+      new_state = %{plots: [new_plot | plots], last_id: new_id}
+      {new_plot, new_state}
+    end)
   end
 
   def release(pid, plot_id) do
-    # Please implement the release/2 function
+    Agent.update(pid, fn state ->
+      new_plots = Enum.reject(state.plots, fn plot -> plot.plot_id == plot_id end)
+      %{state | plots: new_plots}
+    end)
   end
 
   def get_registration(pid, plot_id) do
-    # Please implement the get_registration/2 function
+    Agent.get(pid, fn state ->
+      Enum.find(state.plots, {:not_found, "plot is unregistered"}, fn %Plot{plot_id: id} ->
+        id == plot_id
+      end)
+    end)
   end
 end
