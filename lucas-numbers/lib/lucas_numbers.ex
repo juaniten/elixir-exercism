4b254120c7b70ef62fb89a5c918ea1fe043f96e6
Juan Guijarro <juanguijarroferreiro@gmail.com>
2025-07-07 10:05:01 +0200

    added lucas numbers solution

---
+++ lucas-numbers/lib/lucas_numbers.ex
@@ -0,0 +1,13 @@
+defmodule LucasNumbers do
+  @moduledoc """
+  Lucas numbers are an infinite sequence of numbers which build progressively
+  which hold a strong correlation to the golden ratio (φ or ϕ)
+
+  E.g.: 2, 1, 3, 4, 7, 11, 18, 29, ...
+  """
+  def generate(count) when not is_integer(count) or count < 1 do
+    raise ArgumentError, "count must be specified as an integer >= 1"
+  end
+
+  def generate(n), do: Stream.unfold({2, 1}, fn {a, b} -> {a, {b, a + b}} end) |> Enum.take(n)
+end
